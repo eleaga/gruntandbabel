0368612edef82564b7d5449c9166f4eec4e90786
Luiz Henrique Bezerra <lhbezerra@hotmail.com>
2015-08-30 17:02:19 -0300

    Readme

--- dist/Apresentacao-grunt.js
+++ dist/Apresentacao-grunt.js
@@ -38,3 +38,16 @@ function alterandoI() {
 alterandoI();
 console.log(i);
 //# sourceMappingURL=let.es6.js.map
+;"use strict";
+
+var i = 0;
+
+console.log(i + " i acaba de ser criada com 0");
+
+function alterandoI() {
+  var i = 1;
+  console.log(i + " dentro da funcao deve ser 1");
+}
+alterandoI();
+console.log(i + " novamente fora da funcao é 0");
+//# sourceMappingURL=let.js.map
