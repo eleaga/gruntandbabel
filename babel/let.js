41024d09df53f2125847f1dbb7f929d5a74319db
Luiz Henrique Bezerra <lhbezerra@hotmail.com>
2015-08-30 17:02:41 -0300

    Readme

---
+++ babel/let.js
@@ -0,0 +1,12 @@
+var i=0;
+
+console.log(`${i} i acaba de ser criada com 0`);
+
+function alterandoI(){
+  let i = 1
+  console.log(`${i} dentro da funcao deve ser 1`);
+}
+alterandoI();
+console.log(`${i} novamente fora da funcao é 0`);
+
+
---
+++ src/let.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var i = 0;
+
+console.log(i + " i acaba de ser criada com 0");
+
+function alterandoI() {
+  var i = 1;
+  console.log(i + " dentro da funcao deve ser 1");
+}
+alterandoI();
+console.log(i + " novamente fora da funcao é 0");
+//# sourceMappingURL=let.js.map
